7e86804b580f0399a53514694a6bd77c6d50773e
Chris Muntean <chrismunt123@gmail.com>
2024-11-03 02:49:40 -0700

    Updated footer with my photography subdomain link

--- dynamic-loader.js
+++ dynamic-loader.js
@@ -1,17 +1,29 @@
 // Function to load external HTML
 function loadHTML(elementId, url, callback) {
-fetch(url)
-    .then(response => response.text())
-    .then(data => {
-    document.getElementById(elementId).insertAdjacentHTML('beforebegin', data);
-    document.getElementById(elementId).remove();
-    if (callback) callback();
-    });
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).insertAdjacentHTML('beforebegin', data);
+            document.getElementById(elementId).remove();
+            if (callback) callback();
+        });
+}
+
+// Force tooltip activation (for Photography page promo)
+function initializeTooltips() {
+    // Check if we're on the index page
+    if (window.location.pathname === '/' || window.location.pathname.endsWith('/index.html')) {
+        const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        tooltipTriggerList.forEach(tooltipTriggerEl => {
+            const tooltip = new bootstrap.Tooltip(tooltipTriggerEl);
+            tooltip.show(); // Show the tooltip indefinitely
+        });
+    }
 }
 
 // Load header and footer
 loadHTML('header', '../header.html', setActiveNavLink);
-loadHTML('footer', '../footer.html');
+loadHTML('footer', '../footer.html', initializeTooltips);
 
 // Function to set the active nav link
 function setActiveNavLink() {
